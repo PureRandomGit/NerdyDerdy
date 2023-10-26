e174038a98147c65dfa3241d7a33c757ba88a0af
Samuel Styles <67933247+PureRandomGit@users.noreply.github.com>
2023-10-26 18:57:55 -0400

    Update NerdyDerby.ino

--- NerdyDerby.ino
+++ NerdyDerby.ino
@@ -15,8 +15,7 @@ bool racing = false;
 bool lane1Finished = false;
 bool lane2Finished = false;
 bool lane3Finished = false;
-int elapsedTime = 0;
-int timer = 0;
+
 
 void setup() {
     pinMode(button, INPUT_PULLUP);
@@ -43,96 +42,80 @@ void setup() {
     digitalWrite(green3, LOW);
 }
 
-
-
-// Cycles from red to yellow to green then waits for cars to finish.
-void race() {
-
-    // Countdown with lights then sets to yellow
-    lane1("red");
-    lane2("red");
-    lane3("red");
-    Serial.println("Three");
-    delay(1000);
-    lane1("yellow");
-    lane2("yellow");
-    lane3("yellow");
-    Serial.println("Two");
-    delay(1000);
-    lane1("green");
-    lane2("green");
-    lane3("green");
-    digitalWrite(solenoid, HIGH);
-    Serial.println("One");
-    Serial.println("Start");
-    delay(3000);
-    lane1("off");
-    lane2("off");
-    lane3("off");
-    digitalWrite(solenoid, LOW);
-    Serial.println("Solenoid Off");
-
-    end();
-}
-
-void end() {
-  Serial.println("End Began");
-
-  if (place < 4) { // if all lanes finished, no need to read buttons
-    timer = millis();
-    elapsedTime = millis() - timer;
-    Serial.println(elapsedTime);
-    Serial.println("Place Less than 4");
-    while ((( digitalRead(end1) == HIGH) || (digitalRead(end2) == HIGH) || (digitalRead(end3) == HIGH) ) && (elapsedTime <= 5000)) {
-      elapsedTime = millis() - timer;
-      Serial.println(elapsedTime);
-      if (!digitalRead(end1)) {
-        if(place==1) {
-            lane1("green");
-            place++;
-        }
-        else if(place == 2) {
-            lane1("yellow");
-            place++;
-        }
-        else if(place == 3) {
-            lane1("red");
-            place++;
-        }
-      }
-
-      if (!digitalRead(end2)) {
-          if(place==1) {
-              lane2("green");
-              place++;
-          }
-          else if(place == 2) {
-              lane2("yellow");
-              place++;
-          }
-          else if(place == 3) {
-              lane2("red");
-              place++;
-          }
+void loop() {
+    if(digitalRead(button)==LOW && !racing) {
+        lane1("red");
+        lane2("red");
+        lane3("red");
+        Serial.println("Three");
+        delay(1000);
+        lane1("yellow");
+        lane2("yellow");
+        lane3("yellow");
+        Serial.println("Two");
+        delay(1000);
+        lane1("green");
+        lane2("green");
+        lane3("green");
+        digitalWrite(solenoid, HIGH);
+        Serial.println("One");
+        Serial.println("Start");
+        delay(3000);
+        lane1("off");
+        lane2("off");
+        lane3("off");
+        digitalWrite(solenoid, LOW);
+        Serial.println("Solenoid Off");
+        place = 0;
+    } else {
+        if (digitalRead(end1)==LOW && !(lane1Finished)) {
+            if(place==1) {
+                lane1("green");
+                place++;
+            }
+            else if(place == 2) {
+                lane1("yellow");
+                place++;
+            }
+            else if(place == 3) {
+                lane1("red");
+                place++;
+            }
+            lane1Finished = true;
         }
 
-      if (!digitalRead(end3)) {
-        if(place==1) {
-            lane3("green");
-            place++;
+        if (!digitalRead(end2)==LOW && !(lane2Finished)) {
+            if(place==1) {
+                lane2("green");
+                place++;
+            }
+            else if(place == 2) {
+                lane2("yellow");
+                place++;
+            }
+            else if(place == 3) {
+                lane2("red");
+                place++;
+            }
+            lane2Finished = true;
         }
-        else if(place == 2) {
-            lane3("yellow");
-            place++;
+
+        if (!digitalRead(end3)==LOW && !(lane3Finished)) {
+            if(place==1) {
+                lane3("green");
+                place++;
+            }
+            else if(place == 2) {
+                lane3("yellow");
+                place++;
+            }
+            else if(place == 3) {
+                lane3("red");
+                place++;
+            }
+            lane3Finished = true;
         }
-        else if(place == 3) {
-            lane3("red");
-            place++;
-        }
-      }
     }
-    racing = false;
-  }
 }
 
 // Takes color as input and sets lane to color
@@ -188,15 +171,5 @@ void lane3(char color[]) {
     if (color == "off") {
       digitalWrite(red3, LOW);
       digitalWrite(green3, LOW);
-    }
-}
-
-
-void loop() {
-    if ((digitalRead(button) == LOW) && !(racing)) { //Starts race if not in a race and if start button pushed
-      Serial.println("Button Pressed Beginning Race");
-      racing = true;
-      Serial.println(timer);
-      race();
     }
 }
